4cf392f89be5d16c80ce304ec9cced3f5ab05da3
blondinberra <alexgerhardsson@gmail.com>
2020-10-15 19:46:32 +0200

    Part03

--- Tomki_EXTRA/Program.cs
+++ Tomki_EXTRA/Program.cs
@@ -9,39 +9,36 @@ namespace Extra02
         {
             int run = 1;
             string[] cword;
+            
+            int[] numbers = new int[5];
 
             while (run == 1)
             {
                 Console.WriteLine("Hello and welcome to BOGOL");
+
                 Console.WriteLine("Quit by writing 'quit'!");
 
                 string echo = Console.ReadLine();
 
                 Console.Clear();
 
-                
-
                 cword = echo.Split(' ');
-
 
                 for (int i = 0; i < cword.Length; i++)
                 {
-                    
-
                     if (cword[i].Contains("quit"))
                     {
                         run = 0;
                         Console.WriteLine("Press any key to quit");
-
-                    }
-                    else
-                        Console.WriteLine(cword[i]);
+                    }                       
                 }
 
+                int numberToStore = Int32.Parse(cword[3]);
+                numbers[0] = numberToStore;
+
+                Console.WriteLine("variable a now has the value: " + numbers[0]);
 
                 _ = Console.ReadKey();
-
-
             }
 
         }
