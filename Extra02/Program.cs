0b66d765abfdd45132543978eb57aacbee35ab73
blondinberra <alexgerhardsson@gmail.com>
2020-10-15 15:07:55 +0200

    Initial check-in of module Tomki_EXTRA

---
+++ Extra02/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Extra02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello and welcome to BOGOL");
+            Console.WriteLine("Quit by writing 'quit'!");
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Tomki_EXTRA/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Extra02
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int run = 1;
+
+            while (run == 1)
+            {
+                Console.WriteLine("Hello and welcome to BOGOL");
+                Console.WriteLine("Quit by writing 'quit'!");
+
+                string echo = Console.ReadLine();
+
+                Console.Clear();
+
+                if (echo == "quit")
+                {
+                    run = 0;
+                }
+
+                Console.WriteLine(echo);
+
+
+                Console.WriteLine("Press any key to quit");
+
+                Console.ReadKey();
+            }
+
+
+
+
+        }
+    }
+}
